876dd83b299c07db896dbfd455c6a6df9c725c98
Chau Tran <tranthithuychau1993@gmail.com>
2015-05-11 14:01:17 +0700

    updata fontend login

--- login/php/index.php
+++ login/php/index.php
@@ -26,6 +26,7 @@ else
 
 		<link rel="stylesheet" href="../css/bootstrap.min.css" />
 		<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Raleway:200,400,500'/>
+		<link rel='stylesheet' type='text/css' href="../css/materialize.min.css"/>
 		<link rel='stylesheet' type='text/css' href="../css/materialize.css"/>
 		<link rel='stylesheet' type='text/css' href="../css/style.min.css"/>
 
@@ -35,16 +36,16 @@ else
 
 		<nav id="mainNav" class="">
 			<div class="nav-wrapper">
-			  <a href="index.php" class="brand-logo">logo</a>
-			  <ul id="mainmenu" class="right side-nav">
+			  <a href="index.php" class="brand-logo">multipleLib.com</a>
+			  <ul id="nav-mobile" class="right hide-on-med-and-down">
 			    <li data-menuanchor="welcomePage">
-			      <a href="#welcomePage">welcome</a>
+			      <a href="#welcomePage">Welcome</a>
 			    </li>
 			    <li data-menuanchor="discoverPage">
-			      <a href="#discoverPage">discover</a>
+			      <a href="#discoverPage">About Us</a>
 			    </li>
 			    <li data-menuanchor="subscribePage">
-			      <a href="#subscribePage">newsletter</a>
+			      <a href="#subscribePage">Register</a>
 			    </li>
 			  </ul>
 			</div>
@@ -64,36 +65,37 @@ else
 		  </ul>
 		</div>
         
-		<div id="fullpage" class="fullpage-wrapper loaded" style="height: 100%; position: relative; touch-action: none; ">
+		<div id="fullpage" class="fullpage-wrapper loaded" style="height: 100%; position: relative; touch-action: none;">
 
-		<section id="welcome" class="section hierarchical-timig video fp-section fp-table" data-anchor="welcomePage" data-delay="500" style="height: 810px; margin-top: 64px;">
-		<div class="fp-tableCell" style="height:810px; display: block;">
-			<div id="video-poster" style="height: 28vw;">
+		<section id="welcome" class="section hierarchical-timig video fp-section fp-table" 
+		data-anchor="welcomePage" data-delay="500" style="height: 1000px; margin-top: 64px;">
+		<div class="fp-tableCell" style="height:1000px; display: block;">
+			<div id="video-poster" style="height: 30%; display: block;" >
 				<div class="check-inview fadeup">
-					<h1>lifeaweso.me</h1>
+					<h1>Inspiration Job</h1>
 				</div>
-				<div class="check-inview fadeup">
-					<h2>the best place, for your best moments</h2>
+				<div class="center check-inview fadeup">
+<h5><pre>A good education can change anyone. 
+A good teacher can change everything.</pre></h5>
 				</div>
 				
 			</div>
-			<div style="position: absolute; top: 28vw; width: 100%; height: 100%; background: #00BCD4;">
-				<div class="row center" >
+			<div style="position: absolute; top: 30%; width: 100%; height: 100%; background: #0D8DE4;">
+				<div class="row center" style="margin-top: 2%;">
 				    <form class="container center col s4 offset-s4" 
-				    style="background:#FFF; top:30px;">
-						<div class="input-field"  >
-					        <input id="Username" type="text" class="validate">
+				    style="background:#FFF;">
+						<div class="input-field" >
+					        <input id="Username" id="inputMail" type="text" class="validate">
 					        <label for="Username">Username</label>
 				        </div>
 				        <div class="input-field">
 					        <input id="Password" type="password" class="validate">
 					        <label for="Password">Password</label>
 				        </div>
-				         <div class="center">
+				         <div class="center" style="margin-bottom: 2%">
 					        <a href="http://materializecss.com/getting-started.html" id="login-button" 
-					        class="btn waves-effect waves-light light-blue lighten-1"><i class="mdi-file-cloud left"></i>Get Started</a>
+					        class="btn waves-effect waves-light blue"><i class="mdi-file-cloud left"></i>Get Started</a>
 				        </div>
-				        </br>
 				    </form>
 				</div>
 			</div>
@@ -101,7 +103,7 @@ else
 
 		</div>
 		<footer class="">
-		  &copy; lifeaweso.me - a project by <a href="">@_danielep_</a> and <a href="">@Trip314</a>
+		  &copy; multibple choice lib project by <a href="">@_students</a>
 		</footer>
 
 		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
