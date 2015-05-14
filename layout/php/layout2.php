81409a17b5d7c40ac08ceb3099964a46f88943e5
Chau Tran <tranthithuychau1993@gmail.com>
2015-05-14 11:06:25 +0700

    layout 2

---
+++ layout/php/layout2.php
@@ -0,0 +1,56 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile</title>
+	<meta name="description" content="Multiple choice library of HCM University of Technology; add, modify, delete questions and also create .pdf file">
+	<meta name="keywords" content="social network, social, life, awesome, memories, achievements, tracking, life goals">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+	<meta charset="UTF-8">
+
+	<link rel="stylesheet" href="../css/bootstrap.min.css" />
+	<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Raleway:200,400,500'/>
+	<link rel='stylesheet' type='text/css' href="../css/materialize.min.css"/>
+	<link rel='stylesheet' type='text/css' href="../css/materialize.css"/>
+	<link rel='stylesheet' type='text/css' href="../css/style.min.css"/>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript">
+      	$( document ).ready(function(){
+      		$(".button-collapse").sideNav();
+      	})
+    </script>
+</head>
+<body>
+		
+		<nav id="mainNav">
+			<div class="nav-wrapper " style="background-color: #0D8DE4">
+			 
+			  <a class="page-title">Admin Panel</a>
+			</div>
+		</nav>
+
+		
+		<!--
+		<div class="container"><a href="#" data-activates="left-nav" class="button-collapse full"><i class="mdi-navigation-menu blue"></i></a></div>
+      	  <ul id="left-nav" class="side-nav fixed">
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Dashboard</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Profile</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Alert Message</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Faculty</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Subject</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">User</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Setting</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Log Out</a></li>
+        </ul>
+        -->
+
+	<div id="fullpage" class="fullpage-wrapper loaded" style="height: 100%; position: relative; touch-action: none; margin-top:50px;">
+		<section id="subjects">
+			<p>tranchau</p>
+		</section>
+	</div>
+
+  	<script type="text/javascript" src="../js/materialize.min.js"></script>
+  	<script language="javascript" type="text/javascript" src = "../js/bootstrap.js"></script>
+</body>
+</html>
