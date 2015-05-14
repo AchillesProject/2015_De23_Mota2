08c65144350cd98e5aced0b6fabe6ddcffc3ed14
Chau Tran <tranthithuychau1993@gmail.com>
2015-05-14 11:52:15 +0700

    Add question page

---
+++ Versionbackup/Versionbackup/layout2.php
@@ -0,0 +1,90 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=2.0">
+    <title>Dashboard - Deep Blue Admin</title>
+
+      <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+
+    <!-- you need to include the shieldui css and js assets in order for the charts to work -->
+    <link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/shieldui-all.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/all.min.css" />
+    <script type="text/javascript" src="http://www.shieldui.com/shared/components/latest/js/shieldui-all.min.js"></script>
+    <script type="text/javascript" src="http://www.prepbootstrap.com/Content/js/gridData.js"></script>
+
+
+      <link rel="stylesheet" type="text/css" href="css/shCore.css">
+    <link rel="stylesheet" type="text/css" href="css/demo.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css">
+    <style type="text/css" class="init">
+
+    div.dataTables_wrapper {
+        margin-bottom: 0em;
+    }
+
+    </style>
+    <script type="text/javascript" language="javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" language="javascript" src="js/jquery.dataTables.js"></script>
+    <script type="text/javascript" language="javascript" src="js/shCore.js"></script>
+    <script type="text/javascript" language="javascript" src="js/demo.js"></script>
+    <script type="text/javascript" language="javascript" class="init">
+    $(document).ready(function() {
+    var table = $('table.display').dataTable();
+} );
+    </script>
+
+
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/local.css" />
+
+
+    <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Raleway:200,400,500'/>
+    <link rel='stylesheet' type='text/css' href="css/materialize.min.css"/>
+    <link rel='stylesheet' type='text/css' href="css/materialize.css"/>
+    <link rel='stylesheet' type='text/css' href="css/style.min.css"/>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript">
+        $( document ).ready(function(){
+            $(".button-collapse").sideNav();
+        })
+    </script>
+
+
+</head>
+<body>
+		
+		<nav id="mainNav">
+			<div class="nav-wrapper " style="background-color: #0D8DE4">
+			 
+			  <a class="page-title">Admin Panel</a>
+			</div>
+		</nav>
+
+		
+		<!--
+		<div class="container"><a href="#" data-activates="left-nav" class="button-collapse full"><i class="mdi-navigation-menu blue"></i></a></div>
+      	  <ul id="left-nav" class="side-nav fixed">
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Dashboard</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Profile</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Alert Message</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Faculty</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Subject</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">User</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Setting</a></li>
+            <li><a href="about.html" class="waves-effect waves-blue waves-ripple">Log Out</a></li>
+        </ul>
+        -->
+
+	<div id="fullpage" class="fullpage-wrapper loaded" style="height: 100%; position: relative; touch-action: none; margin-top:50px;">
+		
+	</div>
+
+  	<script type="text/javascript" src="../js/materialize.min.js"></script>
+  	<script language="javascript" type="text/javascript" src = "../js/bootstrap.js"></script>
+</body>
+</html>
